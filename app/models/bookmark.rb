40e0351806835287e71797befa11d016d56e7446
alexandre falcao <alexmfal@gmail.com>
2023-01-16 17:51:49 -0300

    feat: home page

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
+
   validates :comment, length: { minimum: 6 }
-  validates_uniqueness_of :movie_id, :scope => %i[list_id]
+  # validates_uniqueness_of :movie_id, :scope => %i[list_id]
+  validates :movie_id, uniqueness: {scope: :list_id, message: "is already in the list"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+
+  root to: "pages#home"
+
   get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
